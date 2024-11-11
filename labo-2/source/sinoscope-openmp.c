8efc5942fc6cc201bc45e98e474f763838bbd378
Abdel <abdel-madjid.santanna@hotmail.com>
2024-11-10 19:06:07 -0500

    [dev] tentative d'optimisation

--- labo-2/source/sinoscope-openmp.c
+++ labo-2/source/sinoscope-openmp.c
@@ -7,37 +7,33 @@
 #include "log.h"
 #include "sinoscope.h"
 
-int sinoscope_image_openmp(sinoscope_t* sinoscope) {
+#include <omp.h>
+
+int sinoscope_image_serial(sinoscope_t* sinoscope) {
     if (sinoscope == NULL) {
         LOG_ERROR_NULL_PTR();
-        return -1;
+        goto fail_exit;
     }
 
-    
-    float px, py;
-    pixel_t pixel;
-    int index;
-    float value;
-
-#pragma omp parallel private(px, py, index, pixel) 
-#pragma omp for schedule(dynamic) reduction(+: value)
-for (int i = 0; i < sinoscope->width; i++) {
-        py    = sinoscope->dy * i - 2 * M_PI;;
+    #pragma omp parallel for collapse(2)
+    for (int i = 0; i < sinoscope->width; i++) {
         for (int j = 0; j < sinoscope->height; j++) {
-            px    = sinoscope->dx * j - 2 * M_PI;;
-            value = 0;
+            float px    = sinoscope->dx * j - 2 * M_PI;
+            float py    = sinoscope->dy * i - 2 * M_PI;
+            float value = 0;
 
             for (int k = 1; k <= sinoscope->taylor; k += 2) {
                 value += sin(px * k * sinoscope->phase1 + sinoscope->time) / k;
                 value += cos(py * k * sinoscope->phase0) / k;
             }
 
-            value = (2 * atan(value)) / M_PI;
+            value = (atan(value) - atan(-value)) / M_PI;
             value = (value + 1) * 100;
 
+            pixel_t pixel;
             color_value(&pixel, value, sinoscope->interval, sinoscope->interval_inverse);
 
-            index = (i * 3) + (j * 3) * sinoscope->width;
+            int index = (i * 3) + (j * 3) * sinoscope->width;
 
             sinoscope->buffer[index + 0] = pixel.bytes[0];
             sinoscope->buffer[index + 1] = pixel.bytes[1];
@@ -46,4 +42,8 @@ for (int i = 0; i < sinoscope->width; i++) {
     }
 
     return 0;
+
+fail_exit:
+    return -1;
 }
+
