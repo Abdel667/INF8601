31b1120ea44177c06d85a8b705812a3b253b1f24
Abdel <abdel-madjid.santanna@hotmail.com>
2024-11-10 19:32:09 -0500

    Revert "[dev] tentative d'optimisation"
    This reverts commit 8efc5942fc6cc201bc45e98e474f763838bbd378.

--- labo-2/source/sinoscope-openmp.c
+++ labo-2/source/sinoscope-openmp.c
@@ -7,33 +7,37 @@
 #include "log.h"
 #include "sinoscope.h"
 
-#include <omp.h>
-
-int sinoscope_image_serial(sinoscope_t* sinoscope) {
+int sinoscope_image_openmp(sinoscope_t* sinoscope) {
     if (sinoscope == NULL) {
         LOG_ERROR_NULL_PTR();
-        goto fail_exit;
+        return -1;
     }
 
-    #pragma omp parallel for collapse(2)
-    for (int i = 0; i < sinoscope->width; i++) {
+    
+    float px, py;
+    pixel_t pixel;
+    int index;
+    float value;
+
+#pragma omp parallel private(px, py, index, pixel) 
+#pragma omp for schedule(dynamic) reduction(+: value)
+for (int i = 0; i < sinoscope->width; i++) {
+        py    = sinoscope->dy * i - 2 * M_PI;;
         for (int j = 0; j < sinoscope->height; j++) {
-            float px    = sinoscope->dx * j - 2 * M_PI;
-            float py    = sinoscope->dy * i - 2 * M_PI;
-            float value = 0;
+            px    = sinoscope->dx * j - 2 * M_PI;;
+            value = 0;
 
             for (int k = 1; k <= sinoscope->taylor; k += 2) {
                 value += sin(px * k * sinoscope->phase1 + sinoscope->time) / k;
                 value += cos(py * k * sinoscope->phase0) / k;
             }
 
-            value = (atan(value) - atan(-value)) / M_PI;
+            value = (2 * atan(value)) / M_PI;
             value = (value + 1) * 100;
 
-            pixel_t pixel;
             color_value(&pixel, value, sinoscope->interval, sinoscope->interval_inverse);
 
-            int index = (i * 3) + (j * 3) * sinoscope->width;
+            index = (i * 3) + (j * 3) * sinoscope->width;
 
             sinoscope->buffer[index + 0] = pixel.bytes[0];
             sinoscope->buffer[index + 1] = pixel.bytes[1];
@@ -42,8 +46,4 @@ int sinoscope_image_serial(sinoscope_t* sinoscope) {
     }
 
     return 0;
-
-fail_exit:
-    return -1;
 }
-
